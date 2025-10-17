928ab5532bc3d2cf532163c1e265c3153d9ac774
KhoaND <khoa.qianyinga@gmail.com>
2025-10-17 20:53:02 +0700

    modified order service

--- be/src/api/services/order.service.js
+++ be/src/api/services/order.service.js
@@ -2,7 +2,7 @@ import { Op } from 'sequelize';
 import models from '../models/index.js';
 import logger from '../../config/logger.js';
 
-const { Order, Restaurant, GuestSession, RestaurantTable, OrderItem, MenuItem } = models;
+const { Order, Restaurant, GuestSession, RestaurantTable, OrderItem, MenuItem, CustomerVoucher, Voucher, Promotion } = models;
 
 const ORDER_INCLUDES = [
     { model: Restaurant, as: 'restaurant' },
@@ -15,6 +15,14 @@ const ORDER_INCLUDES = [
         model: OrderItem,
         as: 'items',
         include: [{ model: MenuItem, as: 'menuItem' }]
+    },
+    {
+        model: CustomerVoucher,
+        as: 'customerVoucher',
+        include: [
+            { model: Voucher, as: 'voucher' },
+            { model: Promotion, as: 'promotion' }
+        ]
     }
 ];
 
@@ -25,13 +33,45 @@ const toAdminOrder = (orderInstance) => {
 
     const order = orderInstance.get({ plain: true });
 
+    const customerVoucher = order.customerVoucher
+        ? {
+              id: order.customerVoucher.id,
+              code: order.customerVoucher.code,
+              status: order.customerVoucher.status,
+              claimedAt: order.customerVoucher.claimedAt,
+              redeemedAt: order.customerVoucher.redeemedAt,
+              voucher: order.customerVoucher.voucher
+                  ? {
+                        id: order.customerVoucher.voucher.id,
+                        code: order.customerVoucher.voucher.code,
+                        name: order.customerVoucher.voucher.name
+                    }
+                  : null,
+              promotion: order.customerVoucher.promotion
+                  ? {
+                        id: order.customerVoucher.promotion.id,
+                        name: order.customerVoucher.promotion.name,
+                        headline: order.customerVoucher.promotion.headline
+                    }
+                  : null
+          }
+        : null;
+
     return {
         id: order.id,
         shortCode: order.id.slice(0, 8).toUpperCase(),
         status: order.status,
         totalCents: order.totalCents,
+        subtotalCents: order.totalCents + (order.discountAppliedCents || 0),
+        discountAppliedCents: order.discountAppliedCents || 0,
+        voucherDiscountCents: order.voucherDiscountCents || 0,
+        loyaltyDiscountCents: order.loyaltyDiscountCents || 0,
+        loyaltyPointsRedeemed: order.loyaltyPointsRedeemed || 0,
+        earnedLoyaltyPoints: order.earnedLoyaltyPoints || 0,
         placedAt: order.placedAt,
         specialRequest: order.specialRequest,
+        promotionId: order.promotionId || null,
+        customerVoucher,
         restaurant: order.restaurant
             ? {
                   id: order.restaurant.id,
