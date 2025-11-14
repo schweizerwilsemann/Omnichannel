9b12741b1e7b5defcab3f4df4e6073521b09ad42
KhoaND <khoanguyenduc99@gmail.com>
2025-11-14 19:19:30 +0700

    update index file

--- be/src/api/routes/index.js
+++ be/src/api/routes/index.js
@@ -5,6 +5,8 @@ import assetRoutes from './asset.routes.js';
 import orderRoutes from './order.routes.js';
 import customerRoutes from './customer.routes.js';
 import managementRoutes from './management.routes.js';
+import restaurantRoutes from './restaurant.routes.js';
+import expirationRoutes from './expiration.routes.js';
 
 const router = Router();
 
@@ -14,6 +16,7 @@ router.use('/assets', assetRoutes);
 router.use('/orders', orderRoutes);
 router.use('/customer', customerRoutes);
 router.use('/management', managementRoutes);
+router.use('/restaurants', restaurantRoutes);
+router.use('/expiration', expirationRoutes);
 
 export default router;
-
